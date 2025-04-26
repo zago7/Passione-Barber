4f93ec5e54819bb583b0c0364aa8ef95df406325
João Gabriel Mendes <joaogabrielmendes047@gmail.com>
2025-04-26 14:08:01 -0300

    Ajustes na nav-bar na page home e nas seçôes direcionando

--- src/clients/BluServer/src/app/app.component.ts
+++ src/clients/BluServer/src/app/app.component.ts
@@ -7,28 +7,10 @@ import { CarrosselComponent } from './components/carrossel/carrossel.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NavBarComponent, CarrosselComponent],
+  imports: [CommonModule, RouterOutlet, ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'BluServer';
-
-  showCarrossel = true;
-  showNavbar = true;
-
-
-  constructor(private router: Router){
-    this.router.events.subscribe(event => {
-      if(event instanceof NavigationEnd){
-        if(event.url === '/login'){
-          this.showCarrossel = false;
-           this.showNavbar = false;
-        } else {
-          this.showCarrossel = true;
-          this.showNavbar = true;
-        }
-      }
-    })
-  }
 }
--- src/clients/BluServer/src/app/pages/home/home.component.ts
+++ src/clients/BluServer/src/app/pages/home/home.component.ts
@@ -3,14 +3,34 @@ import { NavBarComponent } from "../../components/nav-bar/nav-bar.component";
 import { CardComponent } from "../../components/card/card.component";
 import { CardGrandeComponent } from "../../components/card-grande/card-grande.component";
 import { CardServicosComponent } from "../../components/card-servicos/card-servicos.component";
+import { CarrosselComponent } from "../../components/carrossel/carrossel.component";
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavBarComponent, CardComponent, CardGrandeComponent, CardServicosComponent],
+  imports: [NavBarComponent, CardComponent, CardGrandeComponent, CardServicosComponent, CarrosselComponent,CommonModule,RouterOutlet],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+ showCarrossel = true;
+  showNavbar = true;
 
+
+  constructor(private router: Router){
+    this.router.events.subscribe(event => {
+      if(event instanceof NavigationEnd){
+        if(event.url === '/login'){
+          this.showCarrossel = false;
+           this.showNavbar = false;
+        } else {
+          this.showCarrossel = true;
+          this.showNavbar = true;
+        }
+      }
+    })
+  }
 }
