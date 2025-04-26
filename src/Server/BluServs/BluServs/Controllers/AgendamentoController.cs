e1a30bf3384c0d4f84ad3e4124e09bdaa4137d87
Julio Martini <julio.cm06@gmail.com>
2025-04-26 15:53:49 -0300

    Criação das Models e Controller

---
+++ src/Server/BluServs/BluServs/Controllers/AgendamentoController.cs
@@ -0,0 +1,27 @@
+﻿using BluServs.Infra.Models;
+using BluServs.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BluServs.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AgendamentoController : ControllerBase
+    {
+        private static List<Agendamento> agendamentos = new List<Agendamento>();
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Agendamento>> Get()
+        {
+            return Ok(agendamentos);
+        }
+
+        [HttpPost]
+        public ActionResult<Agendamento> Post([FromBody] Agendamento agendamento)
+        {
+            agendamento.Id = agendamentos.Count + 1; 
+            agendamentos.Add(agendamento);
+            return CreatedAtAction(nameof(Get), new { id = agendamento.Id }, agendamento);
+        }
+    }
+}
