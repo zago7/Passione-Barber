46618f62717c4f38749cf3809aa2413f5432afb1
zago <guilhermebzago10@gmail.com>
2025-04-26 18:37:44 -0300

    Tela de serviços

--- src/clients/BluServer/src/app/components/card-servicos/card-servicos.component.ts
+++ src/clients/BluServer/src/app/components/card-servicos/card-servicos.component.ts
@@ -1,13 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card-servicos',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './card-servicos.component.html',
   styleUrl: './card-servicos.component.css'
 })
 export class CardServicosComponent {
-  @Input() title: string = ''
-  @Input() content: string = ''
+  @Input() title: string = '';
+  @Input() content: string = '';
+  @Input() imageUrl: string = '';
 }
