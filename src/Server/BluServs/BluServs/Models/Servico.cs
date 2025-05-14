db002f7ee836eb19318faf452f37b28ec10232e1
João Gabriel Mendes <joaogabrielmendes047@gmail.com>
2025-05-14 18:59:18 -0300

    Primeiro commit com .gitignore corrigido e com controller e model alterados no backend

--- src/Server/BluServs/BluServs/Models/Servico.cs
+++ src/Server/BluServs/BluServs/Models/Servico.cs
@@ -5,11 +5,11 @@ namespace BluServs.Models
     public class Servico
     {
         public int Id { get; set; }
-        public string Nome { get; set; }
+        public string? Nome { get; set; }
         public decimal Preco { get; set; }
         public int Duracao { get; set; }
 
-        public ICollection<Agendamento> Agendamentos { get; set; }
+        public ICollection<Agendamento>? Agendamentos { get; set; }
     }
 }
 
