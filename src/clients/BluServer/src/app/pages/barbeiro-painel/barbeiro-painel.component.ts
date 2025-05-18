73b651bec6e2ba68290f49c94e43eff17083339c
João Gabriel Mendes <joaogabrielmendes047@gmail.com>
2025-05-18 16:21:12 -0300

    Página básica painel de barbeiro

---
+++ src/clients/BluServer/src/app/pages/barbeiro-painel/barbeiro-painel.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-barbeiro-painel',
+  standalone: true,
+  imports: [],
+  templateUrl: './barbeiro-painel.component.html',
+  styleUrl: './barbeiro-painel.component.css'
+})
+export class BarbeiroPainelComponent {
+
+}
